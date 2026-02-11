d59ff1444538c5ec01c1c41ef34f82df6a719fa9
soruparajan <soruparajan555@gmail.com>
2026-02-11 10:04:23 +0530

    tcl

--- Synthesis/run.tcl
+++ Synthesis/run.tcl
@@ -1,1 +1,19 @@
+read_libs /home/install/FOUNDRY/digital/45nm/LIBS/lib/max/slow.lib
+read_hdl traffilight.v
+elaborate
+read_sdc input_constraints.sdc
 
+syn_generic
+syn_map
+syn_opt
+
+write_hdl > traffic_netlist_without.v
+write_sdc  > output_constraints.sdc
+
+gui_show
+
+report timing > traffic_timing.rpt
+report power > traffic_power.rpt
+report area > traffic_cell.rpt
+report gates > traffic_gates.rpt
+   
